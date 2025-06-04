d3eeb63167ca788cd0522568035587890fbdfaac
Wrttnspknbrkn <kchampfameyeh@gmail.com>
2025-06-04 12:35:16 +0000

    Update FeaturedProductsSection.tsx

--- src/components/home/FeaturedProductsSection.tsx
+++ src/components/home/FeaturedProductsSection.tsx
@@ -13,7 +13,7 @@ const FeaturedProductsSection: React.FC = () => {
             <img
               src="/assets/home/desktop/pattern-circles.svg"
               alt=""
-              className="absolute -top-[20px] left-1/2 transform -translate-x-1/2 w-[558px] h-[558px] opacity-[0.15] tablet:-top-[100px] desktop:-top-[100px] desktop:left-[-150px] desktop:transform-none desktop:w-[944px] desktop:h-[944px] desktop:opacity-[0.2]"
+              className="absolute -top-[20px] left-1/2 transform -translate-x-1/2 w-[558px] h-[558px] opacity-[0.15] tablet:-top-[100px] desktop:-top-[100px] desktop:left-[-150px] desktop:transform-none desktop:w-[944px] desktop:h-[944px] desktop:opacity-[0.3]"
             />
           </div>
           
@@ -50,38 +50,32 @@ const FeaturedProductsSection: React.FC = () => {
             </div>
 
             {/* Desktop Layout - Side by Side */}
-            <div className="hidden desktop:block desktop:h-full">
-              <div className="desktop:grid desktop:grid-cols-2 desktop:h-full desktop:items-end">
-                {/* Left Column - Speaker Image */}
-                <div className="desktop:flex desktop:justify-center desktop:items-end desktop:pb-0 desktop:pl-[117px] desktop:h-full desktop:relative">
-                  <div className="desktop:absolute desktop:bottom-0 desktop:flex desktop:justify-center desktop:w-full">
-                    <ResponsiveImage
-                      mobile="/assets/home/mobile/image-speaker-zx9.png"
-                      tablet="/assets/home/tablet/image-speaker-zx9.png"
-                      desktop="/assets/home/desktop/image-speaker-zx9.png"
-                      alt="ZX9 Speaker"
-                      className="desktop:w-[410px] desktop:h-[493px] desktop:object-contain"
-                    />
-                  </div>
-                </div>
+            <div className="hidden desktop:grid desktop:grid-cols-2 desktop:h-full desktop:items-center">
+              {/* Left Column - Speaker Image */}
+              <div className="flex justify-center items-end pb-[116px] pl-[117px]">
+                <ResponsiveImage
+                  mobile="/assets/home/mobile/image-speaker-zx9.png"
+                  tablet="/assets/home/tablet/image-speaker-zx9.png"
+                  desktop="/assets/home/desktop/image-speaker-zx9.png"
+                  alt="ZX9 Speaker"
+                  className="w-[410px] h-[493px] object-contain"
+                />
+              </div>
 
-                {/* Right Column - Text Content */}
-                <div className="desktop:flex desktop:flex-col desktop:justify-center desktop:text-left desktop:pr-[95px] desktop:h-full">
-                  <div>
-                    <h1 className="text-white text-[56px] font-bold leading-[58px] tracking-[2px] uppercase mb-6">
-                      ZX9<br />SPEAKER
-                    </h1>
-                    <p className="text-white text-[15px] leading-[25px] opacity-75 mb-10 max-w-[349px]">
-                      Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.
-                    </p>
-                    <Link
-                      to="/product/zx9-speaker"
-                      className="bg-audiophile-black text-white px-[31px] py-[15px] text-[13px] font-bold uppercase tracking-[1px] hover:bg-audiophile-dark-gray transition-all duration-300 inline-block"
-                    >
-                      See Product
-                    </Link>
-                  </div>
-                </div>
+              {/* Right Column - Text Content */}
+              <div className="text-left pr-[95px] py-[133px]">
+                <h1 className="text-white text-[56px] font-bold leading-[58px] tracking-[2px] uppercase mb-6">
+                  ZX9<br />SPEAKER
+                </h1>
+                <p className="text-white text-[15px] leading-[25px] opacity-75 mb-10 max-w-[349px]">
+                  Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.
+                </p>
+                <Link
+                  to="/product/zx9-speaker"
+                  className="bg-audiophile-black text-white px-[31px] py-[15px] text-[13px] font-bold uppercase tracking-[1px] hover:bg-audiophile-dark-gray transition-all duration-300 inline-block"
+                >
+                  See Product
+                </Link>
               </div>
             </div>
           </div>
