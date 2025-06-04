7a5500cfa92d38b95d64160ea1c54508d0705dd4
Wrttnspknbrkn <kchampfameyeh@gmail.com>
2025-06-04 10:22:47 +0000

    Update HeroSection.tsx

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -61,7 +61,7 @@ const HeroSection: React.FC = () => {
               <img
                 src="/assets/home/desktop/image-hero.jpg"
                 alt="XX99 Mark II Headphones"
-                className="absolute right-[-150px] top-[30%] transform -translate-y-1/2 h-[120%] w-auto object-contain"
+                className="absolute right-[-180px] top-[40%] transform -translate-y-1/2 h-[115%] w-auto object-contain"
                 style={{ maxWidth: 'none' }}
               />
             </div>
